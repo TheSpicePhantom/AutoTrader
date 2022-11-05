9353be59dd14c2cfe4f84d449facd7610963de6c
drmayx <mj6367@gmail.com>
2022-11-05 16:47:21 +0100

    enabled swagger for production

--- Program.cs
+++ Program.cs
@@ -24,11 +24,11 @@ namespace AutoTrader {
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
+            //if (app.Environment.IsDevelopment())
+            //{
             app.UseSwagger();
             app.UseSwaggerUI();
-            }
+            //}
 
             app.UseAuthorization();
 
