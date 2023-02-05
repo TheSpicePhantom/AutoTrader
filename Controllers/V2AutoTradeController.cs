b02c5498074d23515148ba5cd239700f60763df9
Maciej Jankowicz <midnibit@gmail.com>
2023-02-05 21:05:00 +0100

    Made use of new api and set up a base controller to handle V2 autotrade

---
+++ Controllers/V2AutoTradeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using fxcore2;
+using AutoTrader.Helpers;
+
+namespace AutoTrader.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class V2AutoTradeController
+    {
+        public string Token { get; private set; }
+        public string AccountId { get; private set; }
+
+        private readonly IConfiguration _config;
+        private readonly ILogger _logger;
+
+        public V2AutoTradeController(IConfiguration config, ILogger<SocketOperator> logger)
+        {
+            _config = config;
+            _logger = logger;
+
+            this.Token = this._config.GetValue<string>("token");
+            this.AccountId = this._config.GetValue<string>("accountId");
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        { 
+            O2GSession mSession = O2GTransport.createSession();
+            SessionStatusListener statusListener = new(mSession);
+            mSession.subscribeSessionStatus(statusListener);
+            mSession.loginWithToken(this.AccountId, this.Token, "http://www.fxcorporate.com/Hosts.jsp", "Demo");
+
+            if (statusListener.Connected)
+            { }
+
+            mSession.logout();
+            mSession.unsubscribeSessionStatus(statusListener);
+            mSession.Dispose();
+
+            return new OkObjectResult("ok");
+        }
+    }
+}
---
+++ Helpers/SessionStatusListener.cs
@@ -0,0 +1,57 @@
+﻿using fxcore2;
+
+namespace AutoTrader.Helpers
+{
+    public class SessionStatusListener : IO2GSessionStatus
+    {
+        private O2GSession mSession;
+        private object mEvent = new object();
+
+        public bool Connected { get; private set; } = false;
+        public bool Disconnected { get; private set; } = false;
+        public bool Error { get; private set; } = false; 
+
+        public SessionStatusListener(O2GSession session)
+        {
+            mSession = session;
+        }
+
+        public void onLoginFailed(string error)
+        {
+            Console.WriteLine("Login error: " + error);
+            this.Error = true;
+            lock (mEvent)
+                Monitor.PulseAll(mEvent);
+        }
+
+        public void onSessionStatusChanged(O2GSessionStatusCode status)
+        {
+            if (status == O2GSessionStatusCode.Connected)
+                Connected = true;
+            else
+                Connected = false;
+
+            if (status == O2GSessionStatusCode.Disconnected)
+                Disconnected = true;
+            else
+                Disconnected = false;
+
+            if (status == O2GSessionStatusCode.TradingSessionRequested)
+            {
+                throw new NotImplementedException();
+                /*
+                if (Program.SessionID == "")
+                    Console.WriteLine("Argument for trading session ID is missing");
+                else
+                    mSession.setTradingSession(sessionId, Program.Pin);
+                */
+            }
+            else if (status == O2GSessionStatusCode.Connected)
+            {
+                lock (mEvent)
+                    Monitor.PulseAll(mEvent);
+            }
+        }
+
+    }
+}
